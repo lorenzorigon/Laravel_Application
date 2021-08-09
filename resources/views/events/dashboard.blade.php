8d98e09ec83a3a374786b9de7fb89b8844a0b945
Lorenzo Rigon <lorenzocardoso@hotmail.com>
2021-08-09 12:55:23 -0300

    Edição e deleção de eventos apartir da dashboard!

--- resources/views/events/dashboard.blade.php
+++ resources/views/events/dashboard.blade.php
@@ -23,7 +23,14 @@
                             <td scope="row">{{ $loop->index + 1 }}</td>
                             <td><a href="/events/{{$event->id}}">{{$event->title}}</a></td>
                             <td>0</td>
-                            <td><a href="#">Editar</a> <a href="#">Deletar</a></td>
+                            <td>
+                                <a href="/events/edit/{{ $event->id }}" class="btn btn-info edit-btn"><ion-icon name="create-outline"></ion-icon>Editar</a>
+                                <form action="/events/{{$event->id}}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="btn btn-danger delete-btn" type="submit"><ion-icon name="trash-outline"></ion-icon>Deletar</button>
+                                </form>
+                            </td>
                         </tr>
                     @endforeach
                 </tbody>
